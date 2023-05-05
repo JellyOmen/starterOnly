352a17be7ac46e84ea3ec347cd76293019890337
Soheib <101015711+JellyOmen@users.noreply.github.com>
2023-05-05 14:49:35 +0100

    Master

--- JS/modal.js
+++ JS/modal.js
@@ -1,109 +1,117 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  var y = document.getElementById('close-navbar');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+    y.className += 'close-div';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
+    y.className = '';
   }
+}
+// Invisible Div to enable click away to close menu
+function closeNav() {
+  var x = document.getElementById('myTopnav');
+  var y = document.getElementById('close-navbar');
+
+  x.className = 'topnav';
+  y.className = '';
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeBtn = document.querySelector('.close');
+const modalbg = document.querySelector('.bground');
+const modalSubmitted = document.querySelector('.bground_submitted');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
 const form = document.querySelector('form');
 
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-closeBtn.addEventListener('click', closeModal);
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block';
 }
 
 // Close Modal
 function closeModal() {
   modalbg.style.display = 'none';
+  modalSubmitted.style.display = 'none';
 }
+
+const closeBtn = document.querySelector('.close');
+closeBtn.addEventListener('click', closeModal);
+document.querySelector('.close-modal').addEventListener('click', closeModal);
+document.querySelector('.close-x').addEventListener('click', closeModal);
 
 // validate modal form
 
-var firstError = document.getElementById("first1");
-var lastError = document.getElementById("last1");
-var emailError = document.getElementById("email1");
-var birthDateError = document.getElementById("birthdate1");
-var quantityError = document.getElementById("quantity1");
-var locationsError = document.getElementById("locations1");
-var checkboxError = document.getElementById("checkbox");
+const firstError = document.getElementById("first1");
+const lastError = document.getElementById("last1");
+const emailError = document.getElementById("email1");
+const birthDateError = document.getElementById("birthdate1");
+const quantityError = document.getElementById("quantity1");
+const locationsError = document.getElementById("locations1");
+const checkboxError = document.getElementById("checkbox");
+const input = undefined;
 
 
-function validate(input) {
-  var first = document.getElementById("first").value;
+function FirstName(input) {
+  let first = document.getElementById("first").value;
 
-  if (first.length < 2) {
-    firstError.innerHTML = "Please enter 2+ characters for the first name field.";
-    input.style.border = "2px solid #e54858";
+  if (first.length <= 2) {
+    firstError.innerHTML = "Please enter more than 2 characters.";
     return false;
   }
-  if(/^\s*$/.test(input.value)){
-    input.style.border = "2px solid #e54858";
+  if(/^\s*$/.test(input?.value)){
     input.value = '';
   }
   firstError.innerHTML = '';
-  input.style.border = "2px solid #2e8b57";
   return true;
 }
 
 
-function validate1(input){
-  var last = document.getElementById("last").value;
+function LastName(input){
+  let last = document.getElementById("last").value;
 
-  if (last.length < 2) {
-    lastError.innerHTML = "Please enter 2+ characters for the last name field.";
-    input.style.border = "2px solid #e54858";
+  if (last.length <= 2) {
+    lastError.innerHTML = "Please enter more than 2 characters.";
     return false;
   }
-  if(/^\s*$/.test(input.value)){
-    input.style.border = "2px solid #e54858";
+  if(/^\s*$/.test(input?.value)){
     input.value = '';
   }
   lastError.innerHTML = '';
-  input.style.border = "2px solid #2e8b57";
   return true;
 }
 
-function validate2(input){
-  var email = document.getElementById("email").value;
+function Email(input){
+  let email = document.getElementById("email").value;
 
-  if(/^\s*$/.test(input.value)){
+  if(/^\s*$/.test(input?.value)){
     input.value = '';
   }
 
   if (email.length == 0) {
     emailError.innerHTML = "Email is required";
-    input.style.border = "2px solid #e54858";
     return false;
   }
 
   if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
     emailError.innerHTML = "Enter email in the correct format (email@email.com)";
-    input.style.border = "2px solid #e54858";
     return false;
   }
 
   emailError.innerHTML = '';
-  input.style.border = "2px solid #2e8b57";
   return true;
 }
 
-function validate3(input) {
+function Birthdate(input) {
 
-  var birthdate = document.getElementById("birthdate").value;
+  let birthdate = document.getElementById("birthdate").value;
   if(!/^\d{4}-\d{2}-\d{2}$/.test(birthdate)) {
-      return -1;
+      return 0;
   }
   let parts = birthdate.split('-');
   let now = new Date();
@@ -112,51 +120,96 @@ function validate3(input) {
   
   if(year >= currentYear) {
     birthDateError.innerHTML = 'You must enter valid date.';
-    input.style.border = "2px solid #e54858";
     return false;
   }
   if( (currentYear - year) < 18 ){
     birthDateError.innerHTML = 'You are under 18.';
-    input.style.border = "2px solid #e54858";
     return false;
   }
   birthDateError.innerHTML = '';
-  input.style.border = "2px solid #2e8b57";
   return true;
 }
 
-function validate4(input) {
-  var quantity = document.getElementById("quantity").value;
-
-  if (quantity.length == 0) {
-      quantityError.innerHTML = 'Please enter a numeric value.';
-      input.style.border = "2px solid #e54858";
-      return false;
-  }
-  
-  quantityError.innerHTML = '';
-  input.style.border = "2px solid #2e8b57";
-  return true;
-}
-
-function validate5() {
-  var location = document.getElementsByName("location");
-  let currentLocation;
-  for (i = 0; i < location.length; i++) {
-    if (location[i].checked) {
-      currentLocation = location[i].value;
-    }
-  }
-  if (typeof currentLocation == 'undefined') {
-    locationError.innerHTML = 'You must choose one option';
+function Quantity(input) {
+  let quantity = document.getElementById("quantity");
+  if (quantity.value == '' || quantity.value == null ) {
+    quantityError.innerHTML = 'Please enter number of tournaments attended.';
     return false;
   }
-    locationError.innerHTML = '';
+  if (quantity.value == 0 ) {
+    quantityError.innerHTML = 'You need to be at least participated in 1 tournament';
+    return false;
+  }
+    quantityError.innerHTML = '';
+
     return true;
 }
 
+function Location() {
+  let location = document.getElementsByName("location");
+  for(let i=0; i<location.length; i++){
+    if(location[i].checked){
+      locationsError.innerHTML = '';
+      return true;
+    }
+  }
+    locationsError.innerHTML = 'Please select a location';
+    return false;
+}
 
+function Terms() {
+  let terms = document.getElementById("checkbox1");
+  if (terms.checked == false) {
+    checkboxError.innerHTML = 'You must accept the terms and conditions';
+    return false;
+  } else {
+    checkboxError.innerHTML = '';
+    return true;
+  }
+}
 
+let criteriaCheck = false;
+
+function validate() {
+  if (
+    FirstName() === true &&
+    LastName() === true &&
+    Email() === true &&
+    Birthdate() === true &&
+    Quantity() === true &&
+    Location() === true &&
+    Terms() === true
+  ) {
+    criteriaCheck = true;
+    return true;
+  } else {
+    return false;
+  }
+  
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let first = document.getElementById("first");
+  let last = document.getElementById("last");
+  let email = document.getElementById("email");
+  let birthdate = document.getElementById("birthdate");
+  let quantity = document.getElementById("quantity");
+  let location = document.getElementsByName("location");
+  let terms = document.getElementById("checkbox1");
+  validate();
+  if (criteriaCheck == true) {
+    modalbg.style.display = 'none';
+    modalSubmitted.style.display = 'block';
+    first.value = '';
+    last.value = '';
+    email.value = '';
+    birthdate.value = '';
+    quantity.value = '';
+    location.value = '';
+    terms.checked = false;
+  }
+});
 
 
 
